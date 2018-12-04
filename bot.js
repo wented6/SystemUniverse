5bb98d034122db5a9baac7aa172b8b71702892ce
wented6 <44713416+wented6@users.noreply.github.com>
2018-12-05 02:49:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,10 +40,7 @@ client.on('ready', () => {
 
 
 
-
-
 const welcome = JSON.parse(fs.readFileSync('./welcomer.json' , 'utf8'));
- 
 client.on('message', message => {
            if (!message.channel.guild) return;
  
